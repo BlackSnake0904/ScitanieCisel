326ecd44629fcc7a0eb3a6b46f75369f05d12ee1
BlackSnake0904 <117591705+BlackSnake0904@users.noreply.github.com>
2024-09-05 17:40:43 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var sucet = 0;
+        while (true) {
+            Console.WriteLine("zadaj cislo pre sucet alebo napis ok");
+            var input = Console.ReadLine();
+            
+            if (input == "ok")
+            {
+                Console.Write("Wysledok je: ");
+                Console.WriteLine(sucet);
+                break;
+            }
+            else {
+                try
+                {
+                    sucet += Convert.ToInt32(input);
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Zle zadany vstup skus znova");
+                    continue;
+                }
+
+                
+            }
+
+        }
+    }
+}
